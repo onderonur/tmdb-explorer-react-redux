3bfeac242139694e82969e4ca29d0df9b86560a1
onderonur <onuronder.onr@gmail.com>
2019-07-01 01:29:25 +0300

    dialog refactor

---
+++ src/components/BaseDialogTitle.js
@@ -0,0 +1,38 @@
+import React, { useContext } from "react";
+import { DialogContext } from "./BaseDialog";
+import { DialogTitle, Typography, Box, IconButton } from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles(theme => ({
+  dialogTitle: {
+    padding: theme.spacing(1, 2)
+  },
+  closeButton: {
+    marginRight: theme.spacing(1)
+  }
+}));
+
+function BaseDialogTitle({ children }) {
+  const classes = useStyles();
+  const { fullScreen, closeDialog } = useContext(DialogContext);
+
+  return (
+    <DialogTitle className={classes.dialogTitle} disableTypography>
+      <Box display="flex" alignItems="center">
+        {fullScreen && (
+          <IconButton className={classes.closeButton} onClick={closeDialog}>
+            <CloseIcon />
+          </IconButton>
+        )}
+        {typeof children === "string" ? (
+          <Typography variant="h6">{children}</Typography>
+        ) : (
+          children
+        )}
+      </Box>
+    </DialogTitle>
+  );
+}
+
+export default BaseDialogTitle;
--- src/utils/index.js
+++ src/utils/index.js
@@ -37,7 +37,7 @@ export function getImdbProfileUrl(imdbId) {
   return `https://www.imdb.com/title/${imdbId}`;
 }
 
-export function addStateToLocation(to, state = {}) {
+export function addStateToLocation(to, state) {
   let toObject;
 
   if (typeof to === "string") {
