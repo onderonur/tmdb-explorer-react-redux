4a62525e44ffde2e32078a29dc55072560c67a89
Onur ONDER <onur.onder@arkas.com>
2019-07-13 18:21:07 +0300

    Loading indicator to movie videos list

--- src/containers/MovieVideoList.js
+++ src/containers/MovieVideoList.js
@@ -1,29 +1,34 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchMovieVideos } from "actions";
-import { selectMovieVideos } from "reducers";
-import { List } from "@material-ui/core";
-import MovieVideoListItem from "./MovieVideoListItem";
-import MovieVideoPlayerModal from "./MovieVideoPlayerModal";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMovieVideos } from 'actions';
+import { selectMovieVideos } from 'reducers';
+import { List } from '@material-ui/core';
+import MovieVideoListItem from './MovieVideoListItem';
+import MovieVideoPlayerModal from './MovieVideoPlayerModal';
+import LoadingIndicator from 'components/LoadingIndicator';
+import { selectIsFetchingMovieVideos } from 'reducers';
 
 function MovieVideoList({ movieId }) {
   const dispatch = useDispatch();
   const movieVideoIds =
     useSelector(state => selectMovieVideos(state, movieId)) || [];
+  const isFetching = useSelector(state =>
+    selectIsFetchingMovieVideos(state, movieId)
+  );
 
   useEffect(() => {
     dispatch(fetchMovieVideos(movieId));
   }, [dispatch, movieId]);
 
   return (
-    <>
+    <LoadingIndicator loading={isFetching}>
       <List>
         {movieVideoIds.map(videoId => (
           <MovieVideoListItem key={videoId} videoId={videoId} />
         ))}
       </List>
       <MovieVideoPlayerModal movieId={movieId} />
-    </>
+    </LoadingIndicator>
   );
 }
 
