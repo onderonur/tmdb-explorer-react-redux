e70802b5d54160dd5c503003ba76ec8e4bba6dc7
Onur ONDER <onur.onder@arkas.com>
2019-07-15 06:40:10 +0300

    minimal refactor

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
-import React, { useEffect } from "react";
-import { CssBaseline, Container } from "@material-ui/core";
-import Routes from "components/Routes";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchGenres } from "actions";
-import LoadingIndicator from "components/LoadingIndicator";
-import Header from "components/Header";
-import { makeStyles } from "@material-ui/styles";
-import { selectIsFetchingGenres } from "reducers";
-import AppDrawer from "containers/AppDrawer";
-import BackToTopButton from "components/BackToTopButton";
+import React, { useEffect } from 'react';
+import { CssBaseline, Container } from '@material-ui/core';
+import Routes from 'components/Routes';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchGenres } from 'actions';
+import LoadingIndicator from 'components/LoadingIndicator';
+import Header from 'components/Header';
+import { makeStyles } from '@material-ui/styles';
+import { selectIsFetchingGenres } from 'reducers';
+import AppDrawer from 'containers/AppDrawer';
+import BackToTopButton from 'components/BackToTopButton';
 
 const useStyles = makeStyles(theme => ({
   toolbar: theme.mixins.toolbar,
@@ -31,13 +31,13 @@ function App() {
       <CssBaseline />
       <Header />
       <AppDrawer />
+      <div className={classes.toolbar} />
       <Container className={classes.main} component="main">
-        <div className={classes.toolbar} />
-        <BackToTopButton />
         <LoadingIndicator loading={isFetching}>
           <Routes />
         </LoadingIndicator>
       </Container>
+      <BackToTopButton />
     </>
   );
 }
