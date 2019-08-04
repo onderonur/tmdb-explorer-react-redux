5f1188a638ff4934434118b199646ad41bc39193
onderonur <onuronder.onr@gmail.com>
2019-08-04 06:18:08 +0300

    loading indicator fix

--- src/components/LoadingIndicator.js
+++ src/components/LoadingIndicator.js
@@ -2,7 +2,7 @@ import React from "react";
 import { CircularProgress, Box } from "@material-ui/core";
 
 function LoadingIndicator({ loading, children }) {
-  return loading === true ? (
+  return loading ? (
     <Box display="flex" justifyContent="center" my={2} flexGrow={1}>
       <CircularProgress size={48} color="secondary" />
     </Box>
