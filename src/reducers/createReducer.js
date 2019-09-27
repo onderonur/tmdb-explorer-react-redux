2119c99cc4ef859ed0a283ec3603906450b1ffe6
Onur ONDER <onur.onder@arkas.com>
2019-09-27 20:01:20 +0300

    minor refactor

--- src/reducers/createReducer.js
+++ src/reducers/createReducer.js
@@ -4,7 +4,8 @@ function createReducer(initialState, handlers) {
   return function reducer(state = initialState, action) {
     if (handlers.hasOwnProperty(action.type)) {
       // Using immer to allow state mutations to simplify reducers
-      return produce(state, draft => handlers[action.type](draft, action));
+      const caseReducer = handlers[action.type];
+      return produce(state, draft => caseReducer(draft, action));
     } else {
       return state;
     }
