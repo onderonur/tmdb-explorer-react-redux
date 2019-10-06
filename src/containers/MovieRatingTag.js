3c624f98e0da7a4269105d95ce2d512d551e6a2c
onderonur <onuronder.onr@gmail.com>
2019-10-07 01:28:31 +0300

    movie rating tags

---
+++ src/containers/MovieRatingTag.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectMovie } from "reducers";
+import { makeStyles, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+  tag: {
+    backgroundColor: theme.palette.secondary.main,
+    padding: theme.spacing(0.25, 0.5)
+  }
+}));
+
+function MovieRatingTag({ movieId }) {
+  const classes = useStyles();
+  const movie = useSelector(state => selectMovie(state, movieId));
+
+  return (
+    <div className={classes.tag}>
+      <Typography variant="caption">{movie.vote_average} / 10</Typography>
+    </div>
+  );
+}
+
+export default MovieRatingTag;
