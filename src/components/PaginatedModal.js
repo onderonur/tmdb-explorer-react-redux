9bcc2b02da66cb66f1cdb6de5558b65c7991e102
onderonur <onuronder.onr@gmail.com>
2019-10-07 03:55:25 +0300

    paginated modal w/ hotkeys

--- src/components/PaginatedModal.js
+++ src/components/PaginatedModal.js
@@ -5,6 +5,12 @@ import { useTheme } from "@material-ui/styles";
 import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
 import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
 import useHistoryPush from "hooks/useHistoryPush";
+import { HotKeys } from "react-hotkeys";
+
+const keyMap = {
+  NEXT: ["right", "d"],
+  PREVIOUS: ["left", "a"]
+};
 
 function PaginatedModal({
   title,
@@ -32,58 +38,65 @@ function PaginatedModal({
     setIsVisible(isOpen);
   }, [isOpen]);
 
+  const keyHandlers = {
+    NEXT: () => historyPush(nextPath, { keepScrollState: true }),
+    PREVIOUS: () => historyPush(previousPath, { keepScrollState: true })
+  };
+
   return (
-    <BaseDialog
-      title={title}
-      open={isVisible}
-      onClose={handleClose}
-      onExited={handleExited}
-      zeroPaddingContent
-    >
-      {children}
-      <MobileStepper
-        steps={steps}
-        position="static"
-        variant="text"
-        activeStep={activeStep}
-        nextButton={
-          <Button
-            size="small"
-            disabled={!nextPath}
-            onClick={() =>
-              historyPush(nextPath, {
-                keepScrollState: true
-              })
-            }
-          >
-            Next
-            {theme.direction === "rtl" ? (
-              <KeyboardArrowLeft />
-            ) : (
-              <KeyboardArrowRight />
-            )}
-          </Button>
-        }
-        backButton={
-          <Button
-            size="small"
-            disabled={!previousPath}
-            onClick={() =>
-              historyPush(previousPath, {
-                keepScrollState: true
-              })
-            }
-          >
-            {theme.direction === "rtl" ? (
-              <KeyboardArrowRight />
-            ) : (
-              <KeyboardArrowLeft />
-            )}
-            Back
-          </Button>
-        }
-      />
-    </BaseDialog>
+    <HotKeys keyMap={keyMap} handlers={keyHandlers} allowChanges={true}>
+      <BaseDialog
+        title={title}
+        open={isVisible}
+        onClose={handleClose}
+        onExited={handleExited}
+        zeroPaddingContent
+      >
+        {children}
+        <MobileStepper
+          steps={steps}
+          position="static"
+          variant="text"
+          activeStep={activeStep}
+          nextButton={
+            <Button
+              size="small"
+              disabled={!nextPath}
+              onClick={() =>
+                historyPush(nextPath, {
+                  keepScrollState: true
+                })
+              }
+            >
+              Next
+              {theme.direction === "rtl" ? (
+                <KeyboardArrowLeft />
+              ) : (
+                <KeyboardArrowRight />
+              )}
+            </Button>
+          }
+          backButton={
+            <Button
+              size="small"
+              disabled={!previousPath}
+              onClick={() =>
+                historyPush(previousPath, {
+                  keepScrollState: true
+                })
+              }
+            >
+              {theme.direction === "rtl" ? (
+                <KeyboardArrowRight />
+              ) : (
+                <KeyboardArrowLeft />
+              )}
+              Back
+            </Button>
+          }
+        />
+      </BaseDialog>
+    </HotKeys>
   );
 }
 
