3ef0db761bb61b5f87e6a73a5f498dfd7b22b1d8
onderonur <onuronder.onr@gmail.com>
2019-10-10 23:00:02 +0300

    refactor

--- src/reducers/isFetching.js
+++ src/reducers/isFetching.js
@@ -1,7 +1,7 @@
 import { combineReducers } from "redux";
 import * as actionTypes from "constants/actionTypes";
-import createIsFetching from "./createIsFetching";
-import createByKey from "./createByKey";
+import createIsFetching from "./higherOrderReducers/createIsFetching";
+import createByKey from "./higherOrderReducers/createByKey";
 
 const isFetching = combineReducers({
   genres: createIsFetching([
