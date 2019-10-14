69ad0fca311d608c059ea543a84f737cc8dd5a63
onderonur <onuronder.onr@gmail.com>
2019-10-15 00:57:09 +0300

    redux-observable

--- src/reducers/higherOrderReducers/createReducer.js
+++ src/reducers/higherOrderReducers/createReducer.js
@@ -2,9 +2,10 @@ import { produce } from "immer";
 
 function createReducer(initialState, handlers) {
   return function reducer(state = initialState, action) {
-    if (handlers.hasOwnProperty(action.type)) {
+    const { type } = action;
+    if (handlers.hasOwnProperty(type)) {
       // Using immer to allow state mutations to simplify reducers
-      const caseReducer = handlers[action.type];
+      const caseReducer = handlers[type];
       return produce(state, draft => caseReducer(draft, action));
     } else {
       return state;
